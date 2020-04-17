3749c8fa3dd23570d8a9404fa842a7966fbbcd73
VDrews <thedrews@outlook.es>
2020-04-17 17:04:35 +0200

    Problema con git

--- components/CreateSwitch.vue
+++ components/CreateSwitch.vue
@@ -3,6 +3,7 @@
         v-layout#create(v-if="routing == ''")
             v-flex.mr-1.hover(xs6, @click="routing = 'pill'", style="border-radius: 24px; border: 1px solid #00000015; height: 120px")
                 v-layout.pa-2.pt-12.pl-2(column, align-start, justify-end)
+                    lottie(:options="defaultOptions", :height="50", :width="50", v-on:animCreated="handleAnimation")
                     .font-weight-bold Comunidad
                     div Crea comunidades por temáticas
             v-flex.ml-1.hover(xs6, @click="routing = 'card'", style="border-radius: 24px; border: 1px solid #00000015; height: 120px")
@@ -14,16 +15,26 @@
 </template>
 
 <script>
+import lottie from 'vue-lottie'
+import * as animationData from "@/assets/animation.json"
+
 export default {
     components: {
         CreatePill: () => import("@/components/CreatePill"),
         CreatePost: () => import("@/components/CreatePost"),
+        lottie
     },
 
     data() {
         return {
-            routing: ""
+            routing: "",
+            defaultOptions: {animationData: animationData, loop: false},
         }
+    },
+    methods: {
+        handleAnimation: function (anim) {
+            this.anim = anim;
+        },
     }
 }
 </script>
